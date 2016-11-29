4efa5eb455832866565ee003e8fd27b18d65c1ff
bart9k <bart9k@gmail.com>
2016-11-29 13:46:43 +0100

    num 6-9 fix

--- src/rocky/index.js
+++ src/rocky/index.js
@@ -3,61 +3,67 @@ var rocky = require('rocky');
 // my function to draw number based on imput parametr
 function drawNumber(ctx, cx, cy, digit, colorNumber, colorBg) {
 	
+	//clear first
+	ctx.fillStyle = colorBg;
+	ctx.fillRect(cx, cy, 56, 56);
+	
 	// set didget color
 	ctx.fillStyle = colorNumber;
 	if (digit == 1) {
-		ctx.fillRect(cx+14,cy+7,42,56);
+		ctx.fillRect(cx+7,cy,42,56);
 	} else {
-		ctx.fillRect(cx+7,cy+7,56,56);
+		ctx.fillRect(cx,cy,56,56);
 	}
 	
 	// based on didget draw holes to initial sqaure in same colorBg
 	ctx.fillStyle = colorBg;
 	switch(digit) {
 		case 0:
-			ctx.fillRect(cx+21,cy+21,28,28);
+			ctx.fillRect(cx+14,cy+14,28,28);
         break;
 		case 1:
-			ctx.fillRect(cx+14,cy+21,14,28);
-			ctx.fillRect(cx+42,cy+7,14,42);
+			ctx.fillRect(cx+7,cy+14,14,28);
+			ctx.fillRect(cx+42,cy,14,42);
         break;
 		case 2:
-			ctx.fillRect(cx+7,cy+21,42,7);
-			ctx.fillRect(cx+21,cy+42,42,7);
+			ctx.fillRect(cx,cy+14,42,7);
+			ctx.fillRect(cx+14,cy+35,42,7);
         break;
 		case 3:
-			ctx.fillRect(cx+7,cy+21,42,7);
-			ctx.fillRect(cx+7,cy+42,42,7);
+			ctx.fillRect(cx,cy+14,42,7);
+			ctx.fillRect(cx,cy+35,42,7);
 		break;
 		case 4:
-			ctx.fillRect(cx+21,cy+7,28,21);
-			ctx.fillRect(cx+7,cy+42,42,21);
+			ctx.fillRect(cx+14,cy,28,21);
+			ctx.fillRect(cx,cy+35,42,21);
         break;
 		case 5:
-			ctx.fillRect(cx+7,cy+42,42,7);
-			ctx.fillRect(cx+21,cy+21,42,7);
+			ctx.fillRect(cx,cy+35,42,7);
+			ctx.fillRect(cx+14,cy+14,42,7);
         break;
 		case 6:
-			ctx.fillRect(cx+7,cy+21,42,7);
-			ctx.fillRect(cx+7,cy+42,42,7);
+			ctx.fillRect(cx+14,cy+14,42,7);
+			ctx.fillRect(cx+14,cy+35,28,7);
         break;
 		case 7:
-			ctx.fillRect(cx+7,cy+21,42,7);
-			ctx.fillRect(cx+7,cy+42,42,7);
+			ctx.fillRect(cx,cy+14,42,42);
+			//ctx.fillRect(cx,cy+35,42,7);
         break;
 		case 8:
-			ctx.fillRect(cx+7,cy+21,42,7);
-			ctx.fillRect(cx+7,cy+42,42,7);
+			ctx.fillRect(cx+14,cy+14,28,7);
+			ctx.fillRect(cx+14,cy+35,28,7);
         break;
 		case 9:
-			ctx.fillRect(cx+7,cy+21,42,7);
-			ctx.fillRect(cx+7,cy+42,42,7);
+			ctx.fillRect(cx+14,cy+14,28,7);
+			ctx.fillRect(cx,cy+35,42,7);
         break;
     	default:
 		  ctx.fillStyle = 'grey';
 		  ctx.fillRect(cx+7,cy+7,42,42);
 	}
 }
+
+
 
 rocky.on('draw', function(event) {
 	// Get the CanvasRenderingContext2D object
@@ -69,15 +75,12 @@ rocky.on('draw', function(event) {
 	var colorBg = "lime";
 	var colorDigit = "lightblue";
 	
-	// Clear the screen
-	ctx.fillStyle = colorBg;
-	ctx.fillRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
-
+	
 	// Determine the points of the display individual didgets
-	var xLeft = 1;
-	var xRight = 73;
-	var yUp = 1;
-	var yBottom = 73;
+	var xLeft = 8;
+	var xRight = 80;
+	var yUp = 8;
+	var yBottom = 80;
   
 	// Split hours to individual didgets
 	var hours = d.getHours();
@@ -87,12 +90,29 @@ rocky.on('draw', function(event) {
 	var minuteLeft = Math.floor(minutes/10);
 	var minuteRight = minutes%10;
 
+	// Clear the screen
+	// in future clear just digit which needs redraw -> clear func
+	ctx.fillStyle = colorBg;
+	ctx.fillRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
+
 	//Draw hour didgets
 	drawNumber(ctx, xLeft, yUp, hourLeft, colorDigit, colorBg);
 	drawNumber(ctx, xRight, yUp, hourRight, colorDigit, colorBg);
 	// Draw minute didgets
 	drawNumber(ctx, xLeft, yBottom, minuteLeft, colorDigit, colorBg);
 	drawNumber(ctx, xRight, yBottom, minuteRight, colorDigit, colorBg);
+	
+	// Draw minute didgets
+	drawNumber(ctx, xLeft, yBottom, 8, colorDigit, colorBg);
+	drawNumber(ctx, xRight, yBottom, 9, colorDigit, colorBg);
+	
+	// to do
+	// redraw only digit which needs it
+	// on minute ticks 
+	// 		minuteRight needs redraw everytime
+	// 		if minuteRight = 0 minuteLeft needs redraw
+	//		if minuteRight = 0 and minuteLeft = 0, hourRight needs redraw
+	//		if hourRight = 0 and hourLeft = 1, hourRight = 
 	
 	//console.log("minutes: " + d.getMinutes()%10 + " hours: " + Math.floor(d.getMinutes()/10));
 	console.log(d + " Broken: " + hourLeft + " " + hourRight + " " + minuteLeft + " " + minuteRight);
